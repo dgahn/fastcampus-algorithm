a224055ef20712e16b335b34980220fa4053e7fa
dgahn <dgahn@si-analytics.ai>
2021-05-11 01:18:26 +0900

    feat: 단일 싱글 리스트에 추가, 삭제등의 기능 구현

--- linkedlist/src/test/kotlin/me/dgahn/linkedlist/SingleLinkedList.kt
+++ linkedlist/src/test/kotlin/me/dgahn/linkedlist/SingleLinkedList.kt
@@ -1,15 +1,23 @@
 package me.dgahn.linkedlist
 
+private const val HEAD_INDEX = 0
+private const val FIRST_INDEX = 1
+private const val INIT_VALUE = -1
+
 class SingleLinkedList<T> {
     private var head: Node<T>? = null
     private var tail: Node<T>? = null
+    private var lastIndex = INIT_VALUE
 
-    fun add(node: Node<T>) = if (head == null) {
-        head = node
-        tail = node
-    } else {
-        tail!!.next = node
-        tail = node
+    fun add(node: Node<T>) {
+        if (head == null) {
+            head = node
+            tail = node
+        } else {
+            tail?.next = node
+            tail = node
+        }
+        lastIndex++
     }
 
     fun getHead(): Node<T>? = this.head
@@ -28,6 +36,72 @@ class SingleLinkedList<T> {
             node = node.next
         }
         return builder.toString()
+    }
+
+    fun add(index: Int, node: Node<T>) {
+        when {
+            index == HEAD_INDEX -> addHead(node)
+            index > lastIndex -> addTail(node)
+            else -> addMiddle(index, node)
+        }
+        lastIndex++
+    }
+
+    private fun addTail(node: Node<T>) {
+        tail?.next = node
+        tail = node
+    }
+
+    private fun addMiddle(index: Int, node: Node<T>) {
+        var prev = head
+        for (i in FIRST_INDEX until index) {
+            prev = prev?.next
+        }
+        node.next = prev?.next?.next
+        prev?.next = node
+    }
+
+    private fun addHead(node: Node<T>) {
+        node.next = head?.next
+        head = node
+        return
+    }
+
+    fun get(index: Int): Node<T>? {
+        var current = head
+        for (i in FIRST_INDEX..index) {
+            current = current?.next
+        }
+        return current
+    }
+
+    fun remove(index: Int) {
+        when (index) {
+            HEAD_INDEX -> removeHead()
+            lastIndex -> removeTail(index)
+            else -> removeMiddle(index)
+        }
+    }
+
+    private fun removeHead() {
+        head = head?.next
+    }
+
+    private fun removeMiddle(index: Int) {
+        var prev = head
+        for (i in FIRST_INDEX until index) {
+            prev = prev?.next
+        }
+        prev?.next = prev?.next?.next
+    }
+
+    private fun removeTail(index: Int) {
+        var prev = head
+        for (i in FIRST_INDEX until index) {
+            prev = prev?.next
+        }
+        prev?.next = null
+        tail = prev
     }
 
 }
--- linkedlist/src/test/kotlin/me/dgahn/linkedlist/SingleLinkedListTest.kt
+++ linkedlist/src/test/kotlin/me/dgahn/linkedlist/SingleLinkedListTest.kt
@@ -38,4 +38,95 @@ class SingleLinkedListTest {
         assertThat(linkedList.printAll()).isEqualTo("1 -> 100")
     }
 
+    @Test
+    fun `단일 링크드리스트의 중간에 값을 추가할 수 있다`() {
+        val linkedList = SingleLinkedList<Int>()
+        val firstNode = Node(1, null)
+        val hundredNode = Node(100, null)
+        linkedList.add(firstNode)
+        linkedList.add(hundredNode)
+
+        val fiftyNode = Node(50, null)
+        linkedList.add(1, fiftyNode)
+
+        val result = linkedList.get(1)
+        assertThat(result).isEqualTo(fiftyNode)
+    }
+
+    @Test
+    fun `단일 링크드리스트의 처음에 값을 추가할 수 있다`() {
+        val linkedList = SingleLinkedList<Int>()
+        val firstNode = Node(1, null)
+        val hundredNode = Node(100, null)
+        linkedList.add(firstNode)
+        linkedList.add(hundredNode)
+
+        val fiftyNode = Node(50, null)
+        linkedList.add(0, fiftyNode)
+
+        val result = linkedList.get(0)
+        assertThat(result).isEqualTo(fiftyNode)
+    }
+
+    @Test
+    fun `단일 링크드리스트의 끝에 값을 추가할 수 있다`() {
+        val linkedList = SingleLinkedList<Int>()
+        val firstNode = Node(1, null)
+        val hundredNode = Node(100, null)
+        linkedList.add(firstNode)
+        linkedList.add(hundredNode)
+
+        val fiftyNode = Node(50, null)
+        linkedList.add(2, fiftyNode)
+
+        val result = linkedList.get(2)
+        assertThat(result).isEqualTo(fiftyNode)
+        assertThat(linkedList.getTail()).isEqualTo(fiftyNode)
+    }
+
+    @Test
+    fun `단일 링크드리스트의 값을 삭제할 수 있다`() {
+        val linkedList = SingleLinkedList<Int>()
+        val firstNode = Node(1, null)
+        val hundredNode = Node(100, null)
+        val fiftyNode = Node(50, null)
+        linkedList.add(firstNode)
+        linkedList.add(hundredNode)
+        linkedList.add(fiftyNode)
+
+        linkedList.remove(1)
+        assertThat(linkedList.get(1)).isNotEqualTo(hundredNode)
+        assertThat(linkedList.printAll()).isEqualTo("1 -> 50")
+    }
+
+    @Test
+    fun `단일 링크드리스트의 헤드 값을 삭제할 수 있다`() {
+        val linkedList = SingleLinkedList<Int>()
+        val firstNode = Node(1, null)
+        val hundredNode = Node(100, null)
+        val fiftyNode = Node(50, null)
+        linkedList.add(firstNode)
+        linkedList.add(hundredNode)
+        linkedList.add(fiftyNode)
+
+        linkedList.remove(0)
+        assertThat(linkedList.get(0)).isNotEqualTo(firstNode)
+        assertThat(linkedList.printAll()).isEqualTo("100 -> 50")
+    }
+
+    @Test
+    fun `단일 링크드리스트의 tail 값을 삭제할 수 있다`() {
+        val linkedList = SingleLinkedList<Int>()
+        val firstNode = Node(1, null)
+        val hundredNode = Node(100, null)
+        val fiftyNode = Node(50, null)
+        linkedList.add(firstNode)
+        linkedList.add(hundredNode)
+        linkedList.add(fiftyNode)
+
+        linkedList.remove(2)
+        assertThat(linkedList.getTail()).isEqualTo(hundredNode)
+        assertThat(linkedList.printAll()).isEqualTo("1 -> 100")
+    }
+
 }
